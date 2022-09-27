90a749ecdf9c5878bc0c8bc5621f494be05e6955
K.Kharyn Ilunga <95341639+Kharyn1978@users.noreply.github.com>
2022-09-27 22:37:17 +0200

    workimg on my index.php

--- index.php
+++ index.php
@@ -1,5 +1,75 @@
 <?php
 
+session_start();
+
+include("Template/home.html");
+
+if(isset($_POSt['viewTrips'])){
+
+     $hotel = file_get_contents("hotelData.json");//open the json file_get_contents
+     $hotel = json_decode($hotel); //decode the json file 
+
+     $Userinput = array{
+
+        'search' => $_POST['search'],
+        'room' => $_POST['room'],
+        'adults'=> $_POST['adults'],
+        'children' => $_POST['children'],
+
+
+     };
+
+     //append the input
+
+     $hotel = $Userinput->get('hotel');
+     $hotel = json_encode($Userinput,JSON_PRETTY_PRINT); //second parameter display
+       file_put_contents("hotelData.json",$hotel);
+
+}
+
+class hotel{
+
+    // properties
+
+    private $hotelName;
+    private $roomRate;
+    private $offerPrice;
+
+    // setter method 
+
+    public function comparePrices(string $hotelName, int $roomRate, int $offerPrice) {
+          $this->hotelName = $hotelName ;
+          $this->roomRate = $roomRate ;
+          $this->offerPrice = $offerPrice ;
+    }
+
+
+    // getter method 
+
+    public functtion getCheapest(){
+        echo $this -> hotelName.$this -> roomRate .$this -> offerPrice;
+    }
+
+}
+
+$hotel_1 = new hotel();
+$hotel_1 -> comparePrices("Duchesse Francaise",1500 ,750);
+$hotel_1 -> getCheapest();
+
+$hotel_2 = new hotel();
+$hotel_2-> comparePrices("Chez Model", 500,450);
+$hotel_2 -> getCheapest();
+
+$hotel_3 = new hotel();
+$hotel_3 -> comparePrices("Maison Cherel",1300,1196);
+$hotel_3 -> getCheapest();
+
+$hotel_4 = new hotel();
+$hotel_4 -> comparePrices("Beach House", 1100,880);
+$hotel_4-> getCheapest();
+
+
+
 
 
 $hotel = array {
@@ -18,7 +88,7 @@ $hotel = array {
     $hotel_2=>array{
 
         "Name"=>"Chez Model",
-        "Discount"=> 100,
+        "Discount"=> 10,
         "roorRate"  => 500 , 
         "offerPrice"=>function() {
             $offerRate = this.roomrate * ((100 - this.discount)/100);
@@ -29,7 +99,7 @@ $hotel = array {
     $hotel_3=>array{
 
         "Name"=>"Maison Cherel",
-        "Discount"=> 80,
+        "Discount"=> 8,
         "roorRate"  => 1300 , 
         "offerPrice"=>function() {
             $offerRate = this.roomrate * ((100 - this.discount)/100);
@@ -41,8 +111,8 @@ $hotel = array {
     $hotel_4=>array{
 
         "Name"=>"Beach House",
-        "Discount"=> 80,
-        "roorRate"  => 1300 , 
+        "Discount"=> 20,
+        "roorRate"  => 1100 , 
         "offerPrice"=>function() {
             $offerRate = this.roomrate * ((100 - this.discount)/100);
             return $offerRate;
